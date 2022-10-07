63f19497d2d589a3e3f7cc8899ff33391072f4b6
LeeYongwoo-kor <newlyo1205@naver.com>
2022-10-07 23:36:52 +0900

    Add Cart component, update CartPage, load cart's products + order product

---
+++ coffeeShop/src/components/Cart.js
@@ -0,0 +1,61 @@
+import { routeChange } from "../router/router";
+import { removeItem } from "../util/storage";
+
+export default function Cart({ $target, initialState }) {
+  const $component = document.createElement("div");
+  $component.className = "Cart";
+  this.state = initialState;
+
+  $target.appendChild($component);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.getTotalPrice = () => {
+    return this.state.reduce((acc, option) => {
+      return acc + (option.productPrice + option.optionPrice) * option.quantity;
+    }, 0);
+  };
+
+  this.render = () => {
+    $component.innerHTML = `
+        <ul>
+            ${this.state
+              .map(
+                (cartItem) => `
+                <li class="Cart__item">
+                    <img src="${cartItem.imageUrl}" />
+                    <div class="Cart__itemDescription">
+                        <div>${cartItem.productName} ${cartItem.optionName} ${
+                  cartItem.quantity
+                } ea</div>
+                        <div>$ ${
+                          cartItem.productPrice + cartItem.optionPrice
+                        }</div>
+                    </div>
+                </li>
+              `
+              )
+              .join("")}
+        </ul>
+        <div class="Cart__totalPrice">
+            Total $ ${this.getTotalPrice}
+        </div>
+        <button class="OrderButton">Order</button>
+    `;
+
+    return $component;
+  };
+
+  $component.addEventListener("click", (e) => {
+    if (e.target.className === "OrderButton") {
+      alert("Your order is complete!");
+      removeItem("products_cart");
+      routeChange("/");
+    }
+  });
+
+  this.render();
+}
--- coffeeShop/src/pages/CartPage.js
+++ coffeeShop/src/pages/CartPage.js
@@ -1,10 +1,63 @@
+import { routeChange } from "../router/router.js";
+import { request } from "../api/api.js";
+import { getItem } from "../util/storage.js";
+import Cart from "../components/Cart.js";
+
 export default function CartPage({ $target }) {
+  let cartComponent = null;
+
   const $page = document.createElement("div");
   $page.className = "CartPage";
 
-  $page.innerHTML = "<h1>Product Detail</h1>";
+  $page.innerHTML = "<h1>Cart</h1>";
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  const cartData = getItem("products_cart", []);
+  this.state = {
+    products: null,
+  };
 
   this.render = () => {
-    $target.appendChild($page);
+    if (cartData.length === 0) {
+      alert("Your cart is empty!");
+      routeChange("/");
+    } else {
+      $target.appendChild($page);
+
+      if (this.state.products && !cartComponent) {
+        cartComponent = new Cart({
+          $target: $page,
+          initialState: this.state.products,
+        });
+      }
+    }
   };
+
+  this.fetchProducts = async () => {
+    const products = await Promise.all(
+      cartData.map(async (cartItem) => {
+        const product = await request(`/products/${cartItem.productsId}`);
+        const selectedOption = product.productOptions.find(
+          (option) => option.id === cartItem.optionId
+        );
+
+        return {
+          imageUrl: product.imageUrl,
+          productName: product.name,
+          quantity: cartItem.quantity,
+          productPrice: product.price,
+          optionName: selectedOption.name,
+          optionPrice: selectedOption.price,
+        };
+      })
+    );
+
+    this.setState({ products });
+  };
+
+  this.fetchProducts();
 }
