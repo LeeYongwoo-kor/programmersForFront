dc943de96e223c983a53b25229c5aad3664cc0f6
LeeYongwoo-kor <newlyo1205@naver.com>
2022-11-11 20:54:15 +0900

    Modify index root in index.js, Include /js static route in app.js

--- catPhotoAlbum/app.js
+++ catPhotoAlbum/app.js
@@ -9,7 +9,7 @@ var indexRouter = require("./routes/index");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "pages"));
 app.engine("html", require("ejs").renderFile); // for html
 app.set("view engine", "html");
 
@@ -17,9 +17,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/js", express.static(path.join(__dirname, "js")));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- catPhotoAlbum/routes/index.js
+++ catPhotoAlbum/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("./src/index", { title: "index page" });
+  res.render("index", { title: "index page" });
 });
 
 module.exports = router;
