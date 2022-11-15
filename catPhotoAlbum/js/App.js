2592afd46f01a3e98c748ab0b3207f118fe0038e
LeeYongwoo-kor <newlyo1205@naver.com>
2022-11-15 22:11:10 +0900

    Add onBackClick function in App.js

--- catPhotoAlbum/js/App.js
+++ catPhotoAlbum/js/App.js
@@ -1,6 +1,7 @@
 import Breadcrumb from "./components/Breadcrumb.js";
 import Nodes from "./components/Nodes.js";
 import ImageView from "./components/ImageView.js";
+import { requestApi } from "./api/api.js";
 
 export default function App({ $app }) {
   this.state = {
@@ -28,18 +29,53 @@ export default function App({ $app }) {
     },
 
     onClick: async (node) => {
-      if (node.type === "DIRECTORY") {
-        const nextNodes = await request(node.id);
-        this.setState({
-          ...this.state,
-          depth: [...this.state.depth, node],
-          nodes: nextNodes,
-        });
-      } else if (node.type === "FILE") {
-        this.setState({
-          ...this.state,
-          selectedFilePath: node.filePath,
-        });
+      try {
+        if (node.type === "DIRECTORY") {
+          const nextNodes = await requestApi(node.id);
+          this.setState({
+            ...this.state,
+            depth: [...this.state.depth, node],
+            nodes: nextNodes,
+          });
+        } else if (node.type === "FILE") {
+          this.setState({
+            ...this.state,
+            selectedFilePath: node.filePath,
+          });
+        }
+      } catch (e) {
+        throw new Error("onClick(): Error occurred!");
+      }
+    },
+
+    onBackClick: async () => {
+      try {
+        const nextState = { ...this.state };
+        nextState.depth.pop();
+
+        const prevNodeId =
+          nextState.depth.length === 0
+            ? null
+            : nextState.depth[nextState.depth.length - 1].id;
+
+        if (prevNodeId === null) {
+          const rootNodes = await requestApi();
+          this.setState({
+            ...nextState,
+            isRoot: true,
+            nodes: rootNodes,
+          });
+        } else {
+          const prevNodes = await requestApi(prevNodeId);
+
+          this.setState({
+            ...nextNodes,
+            isRoot: false,
+            nodes: prevNodes,
+          });
+        }
+      } catch (e) {
+        throw new Error("onBackClick(): Error occurred!");
       }
     },
   });
@@ -56,14 +92,14 @@ export default function App({ $app }) {
 
   const init = async () => {
     try {
-      const rootNodes = await request();
+      const rootNodes = await requestApi();
       this.setState({
         ...this.state,
         isRoot: true,
         nodes: rootNodes,
       });
     } catch (e) {
-      // error
+      throw new Error("init(): Error occurred!");
     }
   };
 
