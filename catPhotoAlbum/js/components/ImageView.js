f80240b274c259b0e8c954672a6a70511ba74427
LeeYongwoo-kor <newlyo1205@naver.com>
2022-11-16 18:28:16 +0900

    Remove unnecessary constants in ImageView.js

--- catPhotoAlbum/js/components/ImageView.js
+++ catPhotoAlbum/js/components/ImageView.js
@@ -1,6 +1,3 @@
-const IMAGE_PATH_PREFIX =
-  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
-
 export default function ImageView({ $app, initialState }) {
   this.state = initialState;
   this.$target = document.createElement("div");
@@ -15,7 +12,7 @@ export default function ImageView({ $app, initialState }) {
 
   this.render = () => {
     this.$target.innerHTML = `<div class="content">${
-      this.state ? `<Img src="${IMAGE_PATH_PREFIX}${this.state}">` : ""
+      this.state ? `<Img src="${this.state}">` : ""
     }</div>`;
 
     this.$target.style.display = this.state ? "block" : "none";
