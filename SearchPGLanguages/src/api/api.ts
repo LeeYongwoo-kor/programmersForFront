52868b3cc1467cb288454fb7fb148efcaf311d82
LeeYongwoo-kor <newlyo1205@naver.com>
2022-12-09 04:19:53 +0900

    In src/api, fetchedLanguages is divided into fetchedAllLanguages(All info) and fetchedLanguagesByKeyword

--- SearchPGLanguages/src/api/api.ts
+++ SearchPGLanguages/src/api/api.ts
@@ -2,20 +2,33 @@
 import { ISearchPrgLanguagesRslt } from "../types/pLangs";
 import fetch, { Headers, HeadersInit } from "node-fetch";
 
-const cache: { [key: string]: any } = {};
-
 const API_END_POINT =
   "https://parseapi.back4app.com/classes/All_Programming_Languages";
 
-const count = 1;
-const limit = 705;
-const order = "ProgrammingLanguage";
+const LOCAL_API_END_POINT = "http://localhost:3000/api";
 
-async function api<T>(url: string): Promise<T> {
-  // if (!globalThis.fetch) {
-  //   globalThis.fetch = fetch;
-  //   globalThis.Headers = Headers;
-  // }
+async function api<T>(
+  url: string,
+  requestHeaders: HeadersInit = {}
+): Promise<T> {
+  return fetch(url, { headers: requestHeaders })
+    .then(async (res) => {
+      if (!res.ok && res.status !== 404) {
+        throw new Error("Request failed!");
+      }
+
+      const json = await (res.json() as Promise<T>);
+      return json;
+    })
+    .catch((e) => {
+      throw new Error(`Something Wrong! ${e.message}`);
+    });
+}
+
+export async function fetchedAllLanguages() {
+  const count = 1;
+  const limit = 705;
+  const order = "ProgrammingLanguage";
 
   const requestHeaders: HeadersInit = new Headers();
   requestHeaders.append(
@@ -24,31 +37,14 @@ async function api<T>(url: string): Promise<T> {
   );
   requestHeaders.append("X-Parse-Master-Key", process.env.API_MASTER_KEY!);
 
-  return fetch(url, { headers: requestHeaders })
-    .then(async (res) => {
-      if (!res.ok) {
-        console.error("Request Failed");
-      }
-
-      const json = await (res.json() as Promise<T>);
-      cache[url] = json;
-      return json;
-    })
-    .catch((e) => {
-      throw new Error(`Something Wrong! ${e.message}`);
-    });
+  return api<ISearchPrgLanguagesRslt>(
+    `${API_END_POINT}?count=${count}&limit=${limit}&order=${order}`,
+    requestHeaders
+  );
 }
 
-const request = async (url: string) => {
-  if (cache[url]) {
-    return cache[url];
-  }
-
-  return api<ISearchPrgLanguagesRslt>(url);
-};
-
-export async function fetchedLanguages(keyword: string) {
-  return request(
-    `${API_END_POINT}?count=${count}&limit=${limit}&order=${order}`
+export async function fetchedLanguagesByKeyword(keyword: string) {
+  return api<ISearchPrgLanguagesRslt>(
+    `${LOCAL_API_END_POINT}?keyword=${keyword}`
   );
 }
